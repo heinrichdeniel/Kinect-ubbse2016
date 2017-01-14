3329ddafc46d370971bc76584933fa7df1f3dda5
Jozsa Tibold <jozsatibold@yahoo.com>
2017-01-14 12:22:09 +0200

    Right mouse click

--- Kinnect/KinectControl/MousePointer.cs
+++ Kinnect/KinectControl/MousePointer.cs
@@ -17,7 +17,7 @@ namespace KinectControl
     {
         private Brush b;
         private int x = 0, y = 0;
-        private bool action = false;
+        private byte action = 0;
         private double screen = 0;
         private int refresh;
         private bool showPointer = false;
@@ -49,8 +49,7 @@ namespace KinectControl
         public static extern IntPtr GetDesktopWindow();
 
         [DllImport("user32.dll")]
-        public static extern bool InvalidateRect(IntPtr hwnd, IntPtr lpRect, bool
-bErase);
+        public static extern bool InvalidateRect(IntPtr hwnd, IntPtr lpRect, bool bErase);
 
         public void setMouseLocation(int x, int y)
         {
@@ -73,11 +72,24 @@ bErase);
 
 
         }
-        public void setAction(bool action)
+        public void setLeftClick(bool action)
         {
-            if (this.action != action)
+            if (action && this.action != 2)
             {
-                this.action = action;
+                this.action = 2;
+            } else if (!action && this.action == 2){
+                this.action = 0;
+            }
+        }
+
+        public void setRightClick(bool action)
+        {
+            if (action && this.action != 1)
+            {
+                this.action = 1;
+            }
+            else if(!action && this.action == 1){
+                this.action = 0;
             }
         }
 
@@ -99,11 +111,7 @@ bErase);
                            
                             using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
                             {
-                                GraphicsState state = g.Save();
-                                g.FillEllipse(action ? Brushes.Blue : Brushes.Red, (int)(x * screen - 27 / 2), (int)(y * screen - 27 / 2), (int)(27 * screen), (int)(27 * screen));
-                                
-                                
-
+                                g.FillEllipse(action == 0 ? Brushes.Red: action == 1 ? Brushes.LawnGreen : Brushes.Blue, (int)(x * screen - 27 / 2), (int)(y * screen - 27 / 2), (int)(27 * screen), (int)(27 * screen));
                             }
                             
                             ++refresh;
