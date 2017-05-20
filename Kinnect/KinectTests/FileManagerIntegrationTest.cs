4b9616bd09d42965004db04900b47b96dca31512
JOZSATIBOLD\Jozsa <jozsatibold@yahoo.com>
2017-05-20 11:32:22 +0300

    First End2End test(hogyha az)

---
+++ Kinnect/KinectTests/FileManagerIntegrationTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using KinectControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KinectTests
+{
+	[TestClass]
+	public class FileManagerIntegrationTest
+	{
+		[TestMethod]
+		public void CommandSaving_WithNewCommand_CommandWillBeSaved()
+		{
+			Commands.Average avg = new Commands.Average();
+			avg.keyID = 11;
+			avg.time = 3.2f;
+			Random rand = new Random();
+			avg.pointcount = rand.Next(30, 60);
+			avg.avg[0] = new float[avg.pointcount];
+			avg.avg[1] = new float[avg.pointcount];
+			avg.avg[2] = new float[avg.pointcount];
+			avg.timePointCount = new float[avg.pointcount];
+			for (int i = 0; i < avg.time; ++i)
+			{
+				avg.avg[0][i] = (float)rand.NextDouble();
+				avg.avg[1][i] = (float)rand.NextDouble();
+				avg.avg[2][i] = (float)rand.NextDouble();
+				avg.timePointCount[i] = (i > 0 ? avg.timePointCount[i - 1] : 0) + (float)rand.NextDouble() / 3;
+			}
+			FileManager fm = FileManager.getInstance();
+			fm.writeCommand(avg);
+			Commands.Average avg2 = fm.readCommand(avg.keyID);
+			Assert.AreEqual(true, avg2.keyID == avg.keyID && avg.pointcount == avg2.pointcount);
+		}
+	}
+}
---
+++ Kinnect/KinectTests/FileManagerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KinectTests
+{
+    [TestClass]
+    public class FileManagerTest
+    {
+        [TestMethod]
+        public void KeyKommands_isExist_True()
+        {
+            String xmlKeyCommandFileName = "KeyCommands.xml";
+        }
+    }
+}
